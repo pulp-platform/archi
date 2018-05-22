dc821188b3d37fda7d2b974e5edf1f00ab7ca37d
Germain Haugou <germain.haugou@iie.ee.ethz.ch>
2018-05-22 17:53:16 +0200

    Fixed wrong memory map on vega

--- include/archi/chips/vega/memory_map.h
+++ include/archi/chips/vega/memory_map.h
@@ -33,7 +33,7 @@
 #define ARCHI_SOC_EU_OFFSET           0x00006000
 #define ARCHI_FC_ITC_OFFSET           0x00009800
 #define ARCHI_FC_TIMER_OFFSET         0x0000B000
-#define ARCHI_STDOUT_OFFSET           0x0000F000
+#define ARCHI_STDOUT_OFFSET           0x00012000
 
 
 
