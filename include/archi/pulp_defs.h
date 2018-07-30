2683ad0528d8120c35006eed1bcecf081de00097
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-07-30 14:09:30 +0200

    Added new config for oprecompkw stand-alone with small floats

--- include/archi/pulp_defs.h
+++ include/archi/pulp_defs.h
@@ -53,6 +53,7 @@
 #define CHIP_HERO_Z_7045           32
 #define CHIP_OPRECOMPKW_SA         33
 #define CHIP_OPRECOMPKW_SFLOAT     34
+#define CHIP_OPRECOMPKW_SFLOAT_SA  35
 
 #define CORE_OR1K_V1  0
 #define CORE_OR1K_V2  1
