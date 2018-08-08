6e18f0c06ab3e8206aa7ed1d018736a3ccaf49e7
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-08-08 17:06:19 +0200

    Added config for arnold

--- include/archi/pulp_defs.h
+++ include/archi/pulp_defs.h
@@ -54,6 +54,7 @@
 #define CHIP_OPRECOMPKW_SA         33
 #define CHIP_OPRECOMPKW_SFLOAT     34
 #define CHIP_OPRECOMPKW_SFLOAT_SA  35
+#define CHIP_ARNOLD                36
 
 #define CORE_OR1K_V1  0
 #define CORE_OR1K_V2  1
