1ed5912f4f75d0078f88c6616f1555aff66152a0
Germain Haugou <germain.haugou@iis.ee.ethz.ch>
2018-10-04 13:04:02 +0200

    Added bigpulp-juno

--- include/archi/pulp_defs.h
+++ include/archi/pulp_defs.h
@@ -58,6 +58,7 @@
 #define CHIP_BIGPULP_STANDALONE    37
 #define CHIP_GAP_REV1              38
 #define CHIP_USOC_V1               39
+#define CHIP_BIGPULP_JUNO          40
 
 #define CORE_OR1K_V1  0
 #define CORE_OR1K_V2  1
