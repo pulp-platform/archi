d6a2ea3caf296309856537eaf7f193366cf78b4d
Florian Glaser <glaserf@iis.ee.ethz.ch>
2019-04-02 13:45:45 +0200

    Updated properties, memory map and soc_eu_v3 for vivosoc 3.1

--- include/archi/soc_eu/soc_eu_v3.h
+++ include/archi/soc_eu/soc_eu_v3.h
@@ -19,43 +19,43 @@
 #define __ARCHI_SOC_EU_SOC_EU_V3_H__
 
 
-#define SOC_EU_EVENT             0x00
-#define SOC_FC_FIRST_MASK        0x04
-#define SOC_CL_FIRST_MASK        0x24
-#define SOC_PR_FIRST_MASK        0x44
-#define SOC_ERR_FIRST_MASK       0x64
-#define SOC_TIMER_SEL_HI         0x84
-#define SOC_TIMER_SEL_LO         0x88
+#define SOC_EU_EVENT                0x00
+#define SOC_FC_FIRST_MASK           0x10
+#define SOC_CL_FIRST_MASK           0x20
+#define SOC_PR_FIRST_MASK           0x30
+#define SOC_ERR_FIRST_MASK          0x40
+#define SOC_TIMER_SEL_HI            0x08
+#define SOC_TIMER_SEL_LO            0x0C
 
-#define SOC_EU_EVENT_0			0x1
-#define SOC_EU_EVENT_1			0x2
-#define SOC_EU_EVENT_2			0x4
-#define SOC_EU_EVENT_3			0x8
-#define SOC_EU_EVENT_4			0x10
-#define SOC_EU_EVENT_5			0x20
-#define SOC_EU_EVENT_6			0x40
-#define SOC_EU_EVENT_7			0x80
+#define SOC_EU_EVENT_0              0x1
+#define SOC_EU_EVENT_1              0x2
+#define SOC_EU_EVENT_2              0x4
+#define SOC_EU_EVENT_3              0x8
+#define SOC_EU_EVENT_4              0x10
+#define SOC_EU_EVENT_5              0x20
+#define SOC_EU_EVENT_6              0x40
+#define SOC_EU_EVENT_7              0x80
 
-#define SOC_TIMER_SEL_ENABLE_SHIFT 31
-#define SOC_TIMER_SEL_EVT_SHIFT    0
-#define SOC_TIMER_SEL_EVT_WIDTH    8
-#define SOC_TIMER_SEL_EVT_MASK    ((~0U) >> (32 - SOC_TIMER_SEL_EVT_WIDTH))
+#define SOC_TIMER_SEL_ENABLE_SHIFT  31
+#define SOC_TIMER_SEL_EVT_SHIFT     0
+#define SOC_TIMER_SEL_EVT_WIDTH     8
+#define SOC_TIMER_SEL_EVT_MASK      ((~0U) >> (32 - SOC_TIMER_SEL_EVT_WIDTH))
 // #define SOC_TIMER_SEL_EVT_MASK    0xff
 
-#define SOC_TIMER_SEL_ENABLE_DISABLED   	0
-#define SOC_TIMER_SEL_ENABLE_ENABLED   	1
+#define SOC_TIMER_SEL_ENABLE_DISABLED   0
+#define SOC_TIMER_SEL_ENABLE_ENABLED    1
 
-#define SOC_TIMER_SEL_ENABLE_DIS   	(0   << SOC_TIMER_SEL_ENABLE_SHIFT)
-#define SOC_TIMER_SEL_ENABLE_ENA   	(1   << SOC_TIMER_SEL_ENABLE_SHIFT)
-#define SOC_TIMER_SEL_EVT_VAL(val) 	((val) << SOC_TIMER_SEL_EVT_SHIFT)
+#define SOC_TIMER_SEL_ENABLE_DIS        (0   << SOC_TIMER_SEL_ENABLE_SHIFT)
+#define SOC_TIMER_SEL_ENABLE_ENA        (1   << SOC_TIMER_SEL_ENABLE_SHIFT)
+#define SOC_TIMER_SEL_EVT_VAL(val)      ((val) << SOC_TIMER_SEL_EVT_SHIFT)
 
 // related to XX_FIRST_MASK registers
-#define SOC_NB_EVENT_REGS        8
-#define SOC_NB_EVENT_TARGETS     3
+#define SOC_NB_EVENT_REGS       4
+#define SOC_NB_EVENT_TARGETS    3
 
-#define SOC_FC_MASK(x)    (SOC_FC_FIRST_MASK + (x)*4)
-#define SOC_CL_MASK(x)    (SOC_CL_FIRST_MASK + (x)*4)
-#define SOC_PR_MASK(x)    (SOC_PR_FIRST_MASK + (x)*4)
+#define SOC_FC_MASK(x)          (SOC_FC_FIRST_MASK + (x)*4)
+#define SOC_CL_MASK(x)          (SOC_CL_FIRST_MASK + (x)*4)
+#define SOC_PR_MASK(x)          (SOC_PR_FIRST_MASK + (x)*4)
 
 #define ARCHI_SOC_EVENT_PERIPH_EVT_BASE(periph) ((periph)*ARCHI_SOC_EVENT_UDMA_NB_CHANNEL_EVT)
 
