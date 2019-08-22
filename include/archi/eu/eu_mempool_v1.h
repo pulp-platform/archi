0410ab0dda23a6af72c3bf5312abf14276fc2a6d
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2019-08-22 21:43:44 +0200

    Change memory map to support barriers and mutexes for all cores

--- include/archi/eu/eu_mempool_v1.h
+++ include/archi/eu/eu_mempool_v1.h
@@ -20,44 +20,41 @@
 /*
  * Register memory map
  */
+#define EU_MASK_REG_SIZE                    0x80 // Register size to fit mask for all cores (max_nb_cores/8 bytes)
 
 // Global offsets
-#define EU_CORES_AREA_OFFSET                0x0000
-#define EU_CORES_AREA_SIZE                  0x2000 // Should be 0x00010000 for 1024 cores
+#define EU_CORES_AREA_OFFSET                0x00000000
+#define EU_CORES_AREA_SIZE                  0x00010000
 #define EU_CORE_AREA_SIZE_LOG2              6
 #define EU_CORE_AREA_SIZE                   (1<<EU_CORE_AREA_SIZE_LOG2)
-#define EU_BARRIER_AREA_OFFSET              0x2000
-#define EU_BARRIER_AREA_SIZE                0x0200
-#define EU_BARRIER_SIZE_LOG2                5
+#define EU_SW_EVENTS_AREA_BASE              0x00010000
+#define EU_SW_EVENTS_AREA_SIZE              0x00000104
+#define EU_SOC_EVENTS_AREA_OFFSET           0x00010104
+#define EU_SOC_EVENTS_AREA_SIZE             0x00000004
+#define EU_BARRIER_AREA_OFFSET              0x00010200
+#define EU_BARRIER_AREA_SIZE                0x00004000
+#define EU_BARRIER_SIZE_LOG2                10
 #define EU_BARRIER_SIZE                     (1<<EU_BARRIER_SIZE_LOG2)
-#define EU_SW_EVENTS_AREA_BASE              0x2200
-#define EU_SW_EVENTS_AREA_SIZE              0x0100 // Should be 0x0C00
-#define EU_SOC_EVENTS_AREA_OFFSET           0x2300
-#define EU_SOC_EVENTS_AREA_SIZE             0x0080
-#define EU_EXT_EVENT_AREA_OFFSET            0x2380
-#define EU_EXT_EVENT_AREA_SIZE              0x0080
-#define EU_MUTEX_AREA_SIZE_LOG2             6
+#define EU_MUTEX_AREA_SIZE_LOG2             7
 #define EU_MUTEX_AREA_SIZE                  (1<<EU_MUTEX_AREA_SIZE_LOG2)
-#define EU_DISPATCH_AREA_SIZE_LOG2          6
-#define EU_DISPATCH_AREA_SIZE               (1<<EU_DISPATCH_AREA_SIZE_LOG2)
+// #define EU_DISPATCH_AREA_SIZE_LOG2          7
+// #define EU_DISPATCH_AREA_SIZE               (1<<EU_DISPATCH_AREA_SIZE_LOG2)
 
 // Demux offsets
 #define EU_CORE_DEMUX_OFFSET                0x0000
-#define EU_CORE_DEMUX_SIZE                  0x0040
-#define EU_MUTEX_DEMUX_OFFSET               0x00C0
-#define EU_MUTEX_DEMUX_SIZE                 0x0040
-#define EU_DISPATCH_DEMUX_OFFSET            0x0080
-#define EU_DISPATCH_DEMUX_SIZE              0x0040
-#define EU_LOOP_DEMUX_OFFSET                0x0060
-#define EU_LOOP_DEMUX_SIZE                  0x0020
-#define EU_SW_EVENTS_DEMUX_OFFSET           0x0100
-#define EU_SW_EVENTS_DEMUX_SIZE             0x0100
+#define EU_CORE_DEMUX_SIZE                  EU_CORE_AREA_SIZE
+#define EU_MUTEX_DEMUX_OFFSET               EU_CORE_DEMUX_OFFSET + EU_CORE_DEMUX_SIZE
+#define EU_MUTEX_DEMUX_SIZE                 EU_MUTEX_AREA_SIZE
+#define EU_SW_EVENTS_DEMUX_OFFSET           EU_MUTEX_DEMUX_OFFSET + EU_MUTEX_DEMUX_SIZE
+#define EU_SW_EVENTS_DEMUX_SIZE             EU_SW_EVENTS_AREA_SIZE
 #define EU_BARRIER_DEMUX_OFFSET             0x0200
-#define EU_BARRIER_DEMUX_SIZE               0x0200
+#define EU_BARRIER_DEMUX_SIZE               EU_BARRIER_AREA_SIZE
+// #define EU_DISPATCH_DEMUX_OFFSET            EU_BARRIER_DEMUX_OFFSET + EU_BARRIER_AREA_SIZE
+// #define EU_DISPATCH_DEMUX_SIZE              0x0080
 
 // Only when secure extensions are active
-#define EU_SEC_DEMUX_OFFSET                  0x040
-#define EU_SEC_DEMUX_SIZE                    0x040
+#define EU_SEC_DEMUX_OFFSET                  0x4300
+#define EU_SEC_DEMUX_SIZE                    0x0040
 
 #define EU_SEC_MASK                          0x00
 #define EU_SEC_MASK_AND                      0x04
@@ -76,24 +73,21 @@
 #define EU_CORE_BUFFER_MASKED                  0x20
 #define EU_CORE_BUFFER_IRQ_MASKED              0x24
 #define EU_CORE_BUFFER_CLEAR                   0x28
-#define EU_CORE_SW_EVENTS_MASK                 0x2C
-#define EU_CORE_SW_EVENTS_MASK_AND             0x30
-#define EU_CORE_SW_EVENTS_MASK_OR              0x34
+// #define EU_CORE_SW_EVENTS_MASK                 0x2C
+// #define EU_CORE_SW_EVENTS_MASK_AND             0x30
+// #define EU_CORE_SW_EVENTS_MASK_OR              0x34
 #define EU_CORE_EVENT_WAIT                     0x38
 #define EU_CORE_EVENT_WAIT_CLEAR               0x3C
 
 #define EU_CORE_TRIGG_SW_EVENT                 0x0000
-#define EU_CORE_TRIGG_SW_EVENT_REG_SIZE        0x0080 // Size of one software event register (max_nb_cores/8 bytes)
-#define EU_CORE_TRIGG_SW_EVENT_SIZE            0x0100 // Limit to two software event for the moment (0x0400 for 8 sw events)
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT            0x0040 // Ignore for now
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT_SIZE       0x0040 // Ignore for now
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR      0x0080 // Ignore for now
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR_SIZE 0x0040 // Ignore for now
-
-#define EU_CORE_MASK_SEC_IRQ                   0x40
+#define EU_CORE_TRIGG_SW_EVENT_SIZE            0x0004
+#define EU_CORE_TRIGG_SW_EVENT_WAIT            EU_CORE_TRIGG_SW_EVENT + EU_CORE_TRIGG_SW_EVENT_SIZE
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_SIZE       EU_MASK_REG_SIZE
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR      EU_CORE_TRIGG_SW_EVENT_WAIT + EU_CORE_TRIGG_SW_EVENT_WAIT_SIZE
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR_SIZE EU_MASK_REG_SIZE
 
 // ROM area
-#define EU_ROM_EVENT_SOURCE                 0x00
+// #define EU_ROM_EVENT_SOURCE                 0x00
 
 // Soc events area
 #define EU_SOC_EVENTS_CURRENT_EVENT         0x00
@@ -103,37 +97,24 @@
 #define EU_SOC_EVENTS_EVENT_MASK            (((1<<EU_SOC_EVENTS_EVENT_MASK_BITS)-1) << EU_SOC_EVENTS_EVENT_MASK_OFFSET)
 
 // External events area
-#define EU_EXT_EVENT_GEN                    0x00
+// #define EU_EXT_EVENT_GEN                    0x00
 
  // Single barrier
-#define EU_HW_BARR_TRIGGER_MASK             0x00
-#define EU_HW_BARR_STATUS                   0x04
-#define EU_HW_BARR_STATUS_SUMMARY           0x08
-#define EU_HW_BARR_TARGET_MASK              0x0C
-#define EU_HW_BARR_TRIGGER                  0x10
-#define EU_HW_BARR_TRIGGER_SELF             0x14
-#define EU_HW_BARR_TRIGGER_WAIT             0x18
-#define EU_HW_BARR_TRIGGER_WAIT_CLEAR       0x1C
+#define EU_HW_BARR_TRIGGER_MASK             0x000
+#define EU_HW_BARR_STATUS                   EU_HW_BARR_TRIGGER_MASK + EU_MASK_REG_SIZE
+#define EU_HW_BARR_STATUS_SUMMARY           EU_HW_BARR_STATUS + EU_MASK_REG_SIZE
+#define EU_HW_BARR_TARGET_MASK              EU_HW_BARR_STATUS_SUMMARY + EU_MASK_REG_SIZE
+#define EU_HW_BARR_TRIGGER                  EU_HW_BARR_TARGET_MASK + EU_MASK_REG_SIZE
+#define EU_HW_BARR_TRIGGER_SELF             EU_HW_BARR_TRIGGER + EU_MASK_REG_SIZE
+#define EU_HW_BARR_TRIGGER_WAIT             EU_HW_BARR_TRIGGER_SELF + 0x04
+#define EU_HW_BARR_TRIGGER_WAIT_CLEAR       EU_HW_BARR_TRIGGER_WAIT + 0x04
 
 // Dispatch
-#define EU_DISPATCH_FIFO_ACCESS             0x0
-#define EU_DISPATCH_TEAM_CONFIG             0x4
-#define EU_DISPATCH_NB_ELEM                 8
+// #define EU_DISPATCH_FIFO_ACCESS             0x0
+// #define EU_DISPATCH_TEAM_CONFIG             0x4
+// #define EU_DISPATCH_NB_ELEM                 8
 
-// Loop
-#define EU_LOOP_STATE                       0x0
-#define EU_LOOP_START                       0x4
-#define EU_LOOP_END                         0x8
-#define EU_LOOP_INCR                        0xc
-#define EU_LOOP_CHUNK                      0x10
-#define EU_LOOP_EPOCH                      0x14
-#define EU_LOOP_SINGLE                     0x18
-
-#define EU_LOOP_DONE                        0x0
-#define EU_LOOP_LOCKED                      0x1
-#define EU_LOOP_SKIP                        0x2
-
-// Soc barriers
+// Soc barriers ??
 #define EU_SOC_BARRIER_AREA_OFFSET         0x0000
 #define EU_SOC_BARRIER_AREA_SIZE           0x0100
 #define EU_SOC_BARRIER_SIZE_LOG2           3
@@ -158,20 +139,21 @@
  */
 
  // Areas
-#define EU_CORE_AREA_OFFSET_GET(coreId)                 (EU_CORES_AREA_OFFSET + (coreId)*EU_CORE_AREA_SIZE)
-#define EU_CORE_AREA_COREID_GET(offset)                 ((offset)>>EU_CORE_AREA_SIZE_LOG2)
+#define EU_CORE_AREA_OFFSET_GET(coreId)                 (EU_CORES_AREA_OFFSET + ((coreId) << EU_CORE_AREA_SIZE_LOG2))
+#define EU_CORE_AREA_COREID_GET(offset)                 ((offset) >> EU_CORE_AREA_SIZE_LOG2)
+#define EU_MUTEX_AREA_OFFSET_GET(mutex)                 ((mutex) << 2)
+#define EU_MUTEX_AREA_MUTEXID_GET(offset)               ((offset) >> 2)
+#define EU_BARRIER_AREA_OFFSET_GET(barrier)             ((barrier) << EU_BARRIER_SIZE_LOG2)
+#define EU_BARRIER_AREA_BARRIERID_GET(offset)           ((offset) >> EU_BARRIER_SIZE_LOG2)
+
 #define EU_ROM_AREA_OFFSET_GET(coreId)                  (EU_CORE_AREA_OFFSET_GET(coreId) + EU_ROM_AREA_OFFSET)
-#define EU_BARRIER_AREA_OFFSET_GET(barrier)             ((barrier)*EU_BARRIER_SIZE)
-#define EU_BARRIER_AREA_BARRIERID_GET(offset)           (((offset) & (EU_BARRIER_AREA_SIZE - 1)) >> EU_BARRIER_SIZE_LOG2)
-#define EU_MUTEX_AREA_OFFSET_GET(mutex)                 ((mutex)*EU_MUTEX_AREA_SIZE)
-#define EU_MUTEX_AREA_MUTEXID_GET(offset)               (((offset) & (EU_MUTEX_AREA_SIZE - 1)) >> 2)
 #define EU_DISPATCH_AREA_OFFSET_GET(mutex)              ((dispatch)*EU_DISPATCH_AREA_SIZE)
 #define EU_DISPATCH_AREA_DISPATCHID_GET(offset)         (((offset) & (EU_DISPATCH_AREA_SIZE - 1)) >> 3)
 
 // Core area
-#define EU_CORE_TRIGG_SW_EVENT_OFFSET(event)            (EU_CORE_TRIGG_SW_EVENT + ((event)*EU_CORE_TRIGG_SW_EVENT_REG_SIZE))
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT_OFFSET(event)       (EU_CORE_TRIGG_SW_EVENT_WAIT + ((event)*0x4))
-// #define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR_OFFSET(event) (EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR + ((event)*0x4))
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_OFFSET_GET(event)       (EU_CORE_TRIGG_SW_EVENT_WAIT + ((event)*0x4))
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_EVENT_GET(offset)       ((offset) >> 2)
+#define EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR_OFFSET_GET(event) (EU_CORE_TRIGG_SW_EVENT_WAIT_CLEAR + ((event)*0x4))
 
 // ROM area
 #define EU_ROM_EVENT_SOURCE_OFFSET(event)               (EU_ROM_EVENT_SOURCE + ((event)*0x4))
